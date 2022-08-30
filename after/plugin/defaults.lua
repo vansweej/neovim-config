9c74eab44ef7cd8c00d30381c97e700b2fc917dc
Jan Van Sweevelt <vansweej@gmail.com>
2022-08-30 16:26:40 +0200

    configs

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -7,6 +7,9 @@ api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 g.mapleader = " "
 g.maplocalleader = " "
 
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = true
 opt.termguicolors = true -- Enable colors in terminal
 opt.hlsearch = true --Set highlight on search
 opt.number = true --Make line numbers default
@@ -17,9 +20,10 @@ opt.undofile = true --Save undo history
 opt.ignorecase = true --Case insensitive searching unless /C or capital in search
 opt.smartcase = true -- Smart case
 opt.updatetime = 250 --Decrease update time
-opt.signcolumn = "yes" -- Always show sign column
+opt.signcolumn = "yes:1" -- Always show sign column
 opt.clipboard = "unnamedplus" -- Access system clipboard
 opt.timeoutlen = 300    --  Time in milliseconds to wait for a mapped sequence to complete.
+opt.fileformats=unix,dos
 -- Highlight on yank
 vim.cmd [[
   augroup YankHighlight

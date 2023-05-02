39500f29a0c20eef74a26153dc46e90c91fdab68
Jan Van Sweevelt <vansweej@gmail.com>
2023-05-02 17:39:57 +0200

    handled loading of nvim-treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
-require("plugins").setup()
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer"
 
+if fn.isdirectory(install_path) ~= 0 then 
+  vim.cmd [[packadd nvim-treesitter]]
+end
+
+require("plugins").setup()
